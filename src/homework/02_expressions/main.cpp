2f6df01d56b7c86cf5db9622506b1ec7682d33a5
matt-dunlap-acc <dunlap_matt@rocketmail.com>
2022-06-05 12:36:50 -0500

    Homework 2

--- src/homework/02_expressions/main.cpp
+++ src/homework/02_expressions/main.cpp
@@ -1,15 +1,39 @@
 ﻿//write include statements
-
+#include "hwexpressions.h"
+#include<iostream>
+#include<iomanip>
 
 //write namespace using statement for cout
-
+using std::cout;
+using std::cin;
 
 /*
 Call multiply_numbers with 10 and 10 parameter values and display function result
 */
 int main()
 {
+	double meal_amount, 
+			tip_rate, 
+			tip_amount, 
+			tax_amount, 
+			total;
 	
+	cout << "Enter meal amount: ";
+	cin >> meal_amount;
+
+	tax_amount = get_sales_tax_amount(meal_amount);
+
+	cout << "Enter tip rate: ";
+	cin >> tip_rate;
+
+	tip_amount = get_tip_amount(meal_amount, tip_rate);
+
+	total = tip_amount + tax_amount + meal_amount;
+
+	cout << "Meal Amount: " << meal_amount << "\n";
+	cout << "Sales Tax:   " << tax_amount << "\n";
+	cout << "Tip Amount:  " << tip_amount << "\n";
+	cout << "Total:       " << total << "\n";
 
 	return 0;
 }
--- test/examples/02_expressions/02_expressions_tests.cpp
+++ test/examples/02_expressions/02_expressions_tests.cpp
@@ -1,6 +1,7 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
 #include "multi_assign.h"
+#include "hwexpressions.h"
 
 TEST_CASE("Verify Test Configuration", "verification") {
 	REQUIRE(true == true);
