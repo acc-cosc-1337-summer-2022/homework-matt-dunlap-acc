5e56ecde0c80bf0041d8770e85dcb6e8d187f2bf
matt-dunlap-acc <dunlap_matt@rocketmail.com>
2022-06-22 19:09:22 -0500

    hw4

--- src/homework/04_repetition/dna.h
+++ src/homework/04_repetition/dna.h
@@ -1,5 +1,8 @@
-//add include statements
+#include<string>
+using std::string;
 
-/*
-Write prototype for function(s)
-*/
+double get_gc_content(const string& dna);
+
+string get_dna_complement(string dna);
+
+string reverse_string(string dna);
--- src/homework/04_repetition/main.cpp
+++ src/homework/04_repetition/main.cpp
@@ -1,7 +1,9 @@
 //write include statements
-
+#include<iostream>
 //write using statements
-
+using std::string;
+using std::cout;
+using std::cin;
 
 /*
 Write code that prompts user to enter 1 for Get GC Content, 
@@ -10,7 +12,32 @@ DNA string and call either get gc content or get dna complement
 function and display the result. Program runs as long as 
 user enters a y or Y.
 */
-int main() 
-{
+
+int main(){
+
+	int option = 0;
+	string dna = "y";
+	string result = "";
+	
+	while (option != "y" && option != "Y"){
+	
+		cout<<"\nPlease enter 1 for GC Content or 2 for DNA Complement (y or Y to quit): ";
+		cin>>option;
+		
+		if (option == 1 || option == 2){
+			cout<<"\nPlease enter DNA sequence: ";
+			cin>>dna;
+			
+			if (option == 1){
+				result = get_gc_content(dna);
+			} else if (option == 2){
+				result = get_dna_complement(dna);
+			}
+			
+			cout<<"\nResult: "<<result;
+		}
+		
+	}
+	
 	return 0;
 }
